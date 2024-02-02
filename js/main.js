8d1ebfbc051425ee433f8a3c0581cca94d5f23a5
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2024-02-02 11:57:37 -0500

    from end of class

--- js/main.js
+++ js/main.js
@@ -6,8 +6,18 @@
   //add click listener to send a message
   document.querySelector('main > h2').addEventListener('click', handleClick);
 
+  document.querySelector('header > h1').addEventListener('click', () => {
+    //if you click on the h1... send a message to the service worker
+    let msg = {
+      action: 'hello',
+      num: Math.floor(Math.random() * 100) + 1, // 1 - 100
+    };
+    console.log(msg);
+    sendMessage(msg);
+  });
+
   //add message listener to receive messages
-  window.addEventListener('message', receiveMessage);
+  navigator.serviceWorker.addEventListener('message', receiveMessage);
 
   //window.location.assign('some url'); //same as clicking a link for current page
   //window.open('some url', '_blank', {}); //open a new window/tab
@@ -16,10 +26,23 @@
 
 function sendMessage(msg) {
   //send a message to the service worker
+  navigator.serviceWorker.ready.then((reg) => {
+    //reg.active is the current service worker in control
+    reg.active.postMessage(msg);
+  });
 }
 
-function receiveMessage(msg) {
+function receiveMessage(ev) {
   //receive a message from the service worker
+  console.log(ev.data); //data is the message object
+  let p = document.createElement('p');
+  p.textContent = ev.data;
+  document.querySelector('main').append(p);
+
+  // let url = 'http://127.0.0.1:5333/index.html?id=' + ev.data.id; //page reloads
+
+  let url = location.origin + '/#id=' + ev.data.id; //page does NOT reload
+  location.assign(url);
 }
 
 function handleClick(ev) {
--- sw.js
+++ sw.js
@@ -25,15 +25,71 @@ self.addEventListener('fetch', (ev) => {
   //what do we want to cache? fetch? both?
   const request = ev.request;
   const url = new URL(request.url); //to get the parts of the url
+  // https://bob.work.org/folder/file.html?hello=goodbye
+  // protocol host
+  if (url.origin === self.location.origin) {
+    //local file (one of ours)
+    ev.respondWith(
+      fetch(ev.request.url, {
+        method: 'GET',
+      }).catch(console.warn)
+    );
+  } else {
+    //remote from another server
+    //browser sends a fetch(url, {method: 'OPTIONS'})
+    //server responds to the OPTIONS /preflight request with headers that include
+    // access-control-allow-origin: *
+    console.log(url.origin);
+    ev.respondWith(
+      fetch(ev.request.url, {
+        method: 'GET',
+        credentials: 'omit', //solve opaque response with status code 0 issue
+      })
+        .then((resp) => {
+          // console.log(resp.status);
+          // console.log(resp.headers.get('content-type')); //mime-type
+          // console.log(resp.headers.get('content-length')); //file size
+          // console.log(resp.headers.get('access-control-allow-origin'));
+
+          return resp;
+        })
+        .catch(console.warn)
+    );
+  }
 });
 
 self.addEventListener('message', (ev) => {
-  //message received from client
-  console.log(ev.data);
+  //message received from client line 31 in main.js
+  // console.log(ev.data); // ev.data === msg
+  if ('action' in ev.data) {
+    console.log(ev.data.action); //hello
+  }
+  if ('num' in ev.data && Number(ev.data.num) > 50) {
+    console.log(ev.data.num);
+    let msg = {
+      txt: 'Thanks for the really big number',
+      id: ev.source.id,
+    };
+    sendMessage(msg);
+  }
 });
 
 function sendMessage(message) {
+  //self - service worker
+  // self.clients - list of all the tabs & windows using self
+  // .matchAll() Promise containing the list of clients
   return self.clients.matchAll().then((clients) => {
-    clients.forEach((client) => client.postMessage(message));
+    console.log(clients.length, 'clients currently');
+
+    clients.forEach((client) => {
+      // if (client.id !== message.id) {
+      //only to the sender
+      client.postMessage(message);
+      //this is sending the message
+      // } else {
+      //not the sender
+      // client.postMessage('I am NOT talking to you. Shut up.');
+      // }
+    });
   });
 }
