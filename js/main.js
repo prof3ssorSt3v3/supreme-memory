434e6e6f1ea48b8909ac4faf1ae4d15fd922cc16
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2024-02-02 16:00:42 -0500

    end of class for 300

--- js/main.js
+++ js/main.js
@@ -6,8 +6,38 @@
   //add click listener to send a message
   document.querySelector('main > h2').addEventListener('click', handleClick);
 
+  document.getElementById('eduardo').addEventListener('click', (ev) => {
+    //click on the paragraph to replace the current page.
+    //With a new (search) query string - actually reloads the page
+    let urlQ = location.origin + '/index.html?num=' + Math.random().toString(16);
+    //With a new hash value - might not load the page
+    let urlH = location.origin + '/index.html#num=' + Math.random().toString(16); //random HEX string
+
+    location.assign(urlH);
+  });
+
+  window.addEventListener('hashchange', (ev) => {
+    console.log('HASHCHANGE EVENT'); //triggered by the hash value changing
+    // history.pushState({}, null, location.origin + '/yippee/yahoo');
+  });
+  window.addEventListener('popstate', (ev) => {
+    console.log('POPSTATE'); //triggered by a change to what is written in the url
+  });
+
+  //onload - the page loaded
+  //DOMContentLoaded - the DOM loaded again
+  //pageshow - a new page was loaded
+
+  document.querySelector('header > h1').addEventListener('click', (ev) => {
+    let message = {
+      txt: 'Hello from the main.js file',
+      num: Math.floor(Math.random() * 100) + 1, // 1 - 100
+    };
+    sendMessage(message);
+  });
+
   //add message listener to receive messages
-  window.addEventListener('message', receiveMessage);
+  navigator.serviceWorker.addEventListener('message', receiveMessage);
 
   //window.location.assign('some url'); //same as clicking a link for current page
   //window.open('some url', '_blank', {}); //open a new window/tab
@@ -16,10 +46,16 @@
 
 function sendMessage(msg) {
   //send a message to the service worker
+  navigator.serviceWorker.ready.then((reg) => {
+    //send {msg} to the service worker that is in charge "active"
+    console.log('sending msg to sw');
+    reg.active.postMessage(msg);
+  });
 }
 
-function receiveMessage(msg) {
+function receiveMessage(ev) {
   //receive a message from the service worker
+  console.log('MSG', ev.data);
 }
 
 function handleClick(ev) {
--- sw.js
+++ sw.js
@@ -25,15 +25,65 @@ self.addEventListener('fetch', (ev) => {
   //what do we want to cache? fetch? both?
   const request = ev.request;
   const url = new URL(request.url); //to get the parts of the url
+  let isPicsum = url.origin.includes('picsum.photos');
+  if (isPicsum) {
+    console.log(url.origin); // https://picsum.photos
+    ev.respondWith(
+      fetch(url, {
+        method: 'GET',
+        credentials: 'omit', //stops the status:0 and opaque response problem
+      }).catch(return404)
+    );
+  } else if (url.origin.includes('gstatic.com') || url.origin.includes('googleapis.com')) {
+    console.log('You are asking for a google CSS or font file.');
+    ev.respondWith(fetch(ev.request).catch(return404));
+    //we could save these in the CACHE here too
+  } else {
+    ev.respondWith(fetch(ev.request).catch(return404));
+    //send the file that was requested or a 404 error if anything goes wrong
+  }
 });
 
 self.addEventListener('message', (ev) => {
   //message received from client
   console.log(ev.data);
+  //data === line 29 in main.js msg variable
+  if ('txt' in ev.data) {
+    console.log('Message with txt from client', ev.source.id);
+    //ev - message event
+    // ev.source - is the WindowClient object who sent the message
+    // ev.source.id is the id of the WindowClient
+  }
+  if ('num' in ev.data && Number(ev.data.num) > 50) {
+    //if num is a value greater than 50...
+    //send a message back
+    let msg = {
+      num: ev.data.num * 2,
+      sender: ev.source.id,
+    };
+    sendMessage(msg);
+  }
 });
 
 function sendMessage(message) {
+  //message {num: 88, sender: 'jg2jh3j2hg42j3hg42j3h4g2j3gh42j3hg4j'}
+  // self - the service worker
+  // self.clients - access ALL the tabs/windows using this service worker
+  // self.clients.matchAll() - async task to get the list of clients
+
   return self.clients.matchAll().then((clients) => {
-    clients.forEach((client) => client.postMessage(message));
+    console.log('there are', clients.length, 'tabs or windows');
+
+    clients.forEach((client) => {
+      if (client.id === message.sender) {
+        client.postMessage(message);
+      } else {
+        client.postMessage('This message was NOT for you');
+      }
+    });
   });
+}
+
+function return404(err) {
+  return new Response(null, { status: 404 });
 }
